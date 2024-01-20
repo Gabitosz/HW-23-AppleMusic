9801ea856358b0c5816b20232ead5416dac8d986
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-01-20 17:52:43 +0200

    Initial Commit

---
+++ HW-23-AppleMusic/HW_23_AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW_23_AppleMusicApp.swift
+//  HW-23-AppleMusic
+//
+//  Created by Gabriel Zdravkovici on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW_23_AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
