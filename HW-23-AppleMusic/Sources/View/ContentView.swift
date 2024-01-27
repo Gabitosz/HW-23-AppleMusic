f36ad87d88b2be7019fb8f87d6214983aef01c24
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-01-27 17:12:08 +0200

    refactor content view

--- HW-23-AppleMusic/Sources/View/ContentView.swift
+++ HW-23-AppleMusic/Sources/View/ContentView.swift
@@ -8,17 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TabBar()
+            .tint(.red)
         }
-        .padding()
     }
-}
-
 #Preview {
     ContentView()
 }
