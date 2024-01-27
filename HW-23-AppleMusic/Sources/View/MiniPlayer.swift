7db4967a54002125a1c25e2002edd07d2272f749
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-01-27 17:10:58 +0200

    add mini player

---
+++ HW-23-AppleMusic/Sources/View/MiniPlayer.swift
@@ -0,0 +1,63 @@
+//
+//  MiniPlayer.swift
+//  HW-23-AppleMusic
+//
+//  Created by Gabriel Zdravkovici on 27.01.2024.
+//
+
+import SwiftUI
+
+struct MiniPlayer: View {
+    var body: some View {
+        
+        VStack {
+            HStack(spacing: 15) {
+                
+                Image("rihannaDiamonds")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 55, height: 55)
+                    .cornerRadius(15)
+                
+                Text("Rihanna - Diamonds")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+                Spacer(minLength: 0)
+                
+                Button (action: {} , label:  {
+                    
+                    Image(systemName: "play.fill")
+                        .font(.title2)
+                        .foregroundStyle(.black)
+                    
+                })
+                
+                Button (action: {}, label: {
+                    
+                    Image(systemName: "forward.fill")
+                        .font(.title2)
+                        .foregroundStyle(.gray)
+                })
+            }
+            .padding(.horizontal)
+        }
+        .frame(height: 70)
+        .background(
+            
+            VStack(spacing: 0) {
+                
+                BlurView()
+                Divider()
+                    
+            }
+        
+        )
+        .offset(y: -50)
+        
+    }
+}
+
+#Preview {
+    MiniPlayer()
+}
