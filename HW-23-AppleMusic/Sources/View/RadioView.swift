d68af413425a0a8f54fe859512643f260af77c95
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-02-03 23:56:57 +0200

    add radio view elements

--- HW-23-AppleMusic/Sources/View/RadioView.swift
+++ HW-23-AppleMusic/Sources/View/RadioView.swift
@@ -10,43 +10,57 @@ import SwiftUI
 struct RadioView: View {
     
     let radioData = RadioEdition.data
+    let stationData = Station.data
     
     var rows = [
         GridItem(.flexible())
     ]
     
+    var columns = [
+        GridItem(.flexible(), alignment: .leading)
+    ]
+    
     var body: some View {
-        
         NavigationView {
-            ScrollView(.horizontal) {
-                LazyHGrid(rows: rows, spacing: 25) {
-                    ForEach(radioData, id: \.self) { radioEdition in
-                        RadioCell(radioEdition: radioEdition)
-                        
-                        
-                        
-                        //                    VStack {
-                        //                        ForEach(radioData, id: \.self) { radioEdition in
-                        //                            Text(radioEdition.radioTitle)
-                        //                        }
-                        //                    }
-                        //                    Rectangle()
-                        //                        .foregroundColor(.yellow)
-                        //                        .frame(width: 330)
-                        //  Text(radioData[index].radioTitle)
-                        
-                        //                    Image(radioData[index].imageName)
-                        //                        .resizable()
-                        //                        .frame(width: 330)
-                        
+            VStack {
+                ScrollView(.vertical) {
+                    Divider().padding()
+                    HStack {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            
+                            LazyHGrid(rows: rows, spacing: 10) {
+                                ForEach(radioData, id: \.self) { radioEdition in
+                                    RadioCell(radioEdition: radioEdition)
+                                }
+                            }
+                        }.padding(25)
+                            .padding(.top,-30)
+                            .padding(.bottom, -20)
                     }
-                }.padding(30)
+                    
+                    Divider().padding()
+                    
+                    VStack(alignment: .leading) {
+                        Text("Станции")
+                            .font(.title)
+                            .bold()
+                            .padding(.leading)
+                            .padding(.bottom, -10)
+                        
+                        LazyVGrid(columns: columns, spacing: 20) {
+                            ForEach(stationData, id: \.self) { station in
+                                StationCell(station: station)
+                            }
+                        }.padding(20)
+                    }
+                    VStack {
+                        Spacer(minLength: 90)
+                    }
+                }
             }.navigationTitle("Радио")
-            
         }
     }
 }
-
 #Preview {
     RadioView()
 }
