5c08d105c2d20eee0f22fd3fed06ea87beda7122
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-02-06 23:57:22 +0200

    add category detail cell

---
+++ HW-23-AppleMusic/Sources/View/SearchScreen/Cells/CategoryDetailViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryDetailViewCell.swift
+//  HW-23-AppleMusic
+//
+//  Created by Gabriel Zdravkovici on 06.02.2024.
+//
+
+import SwiftUI
+
+struct CategoryDetailViewCell: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryDetailViewCell()
+}
