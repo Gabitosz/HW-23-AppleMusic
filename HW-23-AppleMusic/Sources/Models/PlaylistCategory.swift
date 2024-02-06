f118ecde2d47726a141954adab38a5c8f366cdd1
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-02-06 23:57:07 +0200

    refactor playlist category model

--- HW-23-AppleMusic/Sources/Models/PlaylistCategory.swift
+++ HW-23-AppleMusic/Sources/Models/PlaylistCategory.swift
@@ -12,6 +12,9 @@ struct PlaylistCategory: Identifiable, Hashable {
     var id = UUID()
     var playlistCategoryTitle: String
     var imageName: String
+    var playlistSubTitle: String
+    var playlistTitle: String
+    var playlistDescription: String
     
     var playlistCategoryImage: Image {
         Image(imageName)
@@ -21,18 +24,34 @@ struct PlaylistCategory: Identifiable, Hashable {
 extension PlaylistCategory {
     
     static var data: [PlaylistCategory] = [
-        PlaylistCategory(playlistCategoryTitle: "Зимнее время", imageName: "categorylist1"),
-        PlaylistCategory(playlistCategoryTitle: "Поп на русском", imageName: "categorylist2"),
-        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist3"),
-        PlaylistCategory(playlistCategoryTitle: "Альтернатива", imageName: "categorylist4"),
-        PlaylistCategory(playlistCategoryTitle: "Пространственное звучаение", imageName: "categorylist5"),
-        PlaylistCategory(playlistCategoryTitle: "Хип-Хоп на русском", imageName: "categorylist6"),
-        PlaylistCategory(playlistCategoryTitle: "Зарубежный Хип-Хоп", imageName: "categorylist7"),
-        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist8"),
-        PlaylistCategory(playlistCategoryTitle: "Танцевальный Поп", imageName: "categorylist9"),
-        PlaylistCategory(playlistCategoryTitle: "R&B Соул", imageName: "categorylist10"),
-        PlaylistCategory(playlistCategoryTitle: "Регги", imageName: "categorylist11"),
-        PlaylistCategory(playlistCategoryTitle: "Поп R&B", imageName: "categorylist12"),
+        PlaylistCategory(playlistCategoryTitle: "Зимнее время", imageName: "categorylist1", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Поп на русском", imageName: "categorylist2", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist3", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Альтернатива", imageName: "categorylist4", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Пространственное звучаение", imageName: "categorylist5", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Хип-Хоп на русском", imageName: "categorylist6", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Зарубежный Хип-Хоп", imageName: "categorylist7", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist8", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Танцевальный Поп", imageName: "categorylist9", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "R&B Соул", imageName: "categorylist10", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Регги", imageName: "categorylist11", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        PlaylistCategory(playlistCategoryTitle: "Поп R&B", imageName: "categorylist12", playlistSubTitle: "Some", playlistTitle: "Tin", playlistDescription: "G"),
+        
+        
+        
+        
+//        PlaylistCategory(playlistCategoryTitle: "Зимнее время", imageName: "categorylist1"),
+//        PlaylistCategory(playlistCategoryTitle: "Поп на русском", imageName: "categorylist2"),
+//        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist3"),
+//        PlaylistCategory(playlistCategoryTitle: "Альтернатива", imageName: "categorylist4"),
+//        PlaylistCategory(playlistCategoryTitle: "Пространственное звучаение", imageName: "categorylist5"),
+//        PlaylistCategory(playlistCategoryTitle: "Хип-Хоп на русском", imageName: "categorylist6"),
+//        PlaylistCategory(playlistCategoryTitle: "Зарубежный Хип-Хоп", imageName: "categorylist7"),
+//        PlaylistCategory(playlistCategoryTitle: "Поп", imageName: "categorylist8"),
+//        PlaylistCategory(playlistCategoryTitle: "Танцевальный Поп", imageName: "categorylist9"),
+//        PlaylistCategory(playlistCategoryTitle: "R&B Соул", imageName: "categorylist10"),
+//        PlaylistCategory(playlistCategoryTitle: "Регги", imageName: "categorylist11"),
+//        PlaylistCategory(playlistCategoryTitle: "Поп R&B", imageName: "categorylist12"),
         
     ]
     
