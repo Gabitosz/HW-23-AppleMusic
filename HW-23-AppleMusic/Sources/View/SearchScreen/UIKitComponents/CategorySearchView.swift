5cc96aeee9c5863e7cb3a5ca11f45075f878b93a
Gabitosz <gabriel.zdravkovici313@gmail.com>
2024-02-19 21:41:27 +0200

    add category search view

---
+++ HW-23-AppleMusic/Sources/View/SearchScreen/UIKitComponents/CategorySearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  HW-23-AppleMusic
+//
+//  Created by Gabriel Zdravkovici on 19.02.2024.
+//
+
+import SwiftUI
+import UIKit
+
+struct CategorySearchView: UIViewControllerRepresentable {
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        return CategorySearchViewController()
+    }
+}
